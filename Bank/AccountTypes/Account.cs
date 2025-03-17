fe162d544535e60ddeba22b02952fdf20c698462
Anthony Capobianco <a.capobianco@stag.technofuturtic.education>
2025-03-17 18:08:39 +0100

    Added Equals method that takes in a string

--- Bank/AccountTypes/Account.cs
+++ Bank/AccountTypes/Account.cs
@@ -7,9 +7,24 @@ public class Account(in string number) {
   protected Lock _lock = new();
   
   public override string ToString() => $"Account Number: {Number}\nBalance: {_balance}:";
-  public override bool Equals(object? obj) => obj is Account a && Number == a.Number;
+  /// <summary>
+  /// This overload takes in an account number.
+  /// </summary>
+  /// <param name="number"></param>
+  /// <returns>true if <paramref name="number"/> is equal
+  /// to "P:Bank.Account.Number" of this instance.</returns>
+  public bool Equals(string number)
+    => Number == number.ToUpper();
+  
+  public override bool Equals(object? obj)
+    => obj is Account a && a.Equals(Number);
+  public override int GetHashCode()
+    => Number.GetHashCode();
+  public override string ToString() 
+    => $"Account Number: {Number}\nBalance: {_balance}:";
+  
   public static bool operator ==(in Account self, object? o) => o switch {
-    string s => self.Number == s,
+    string s => self.Equals(s),
     _ => self.Equals(o),
   };
   public static bool operator !=(in Account self, object? o) => !(self == o);
