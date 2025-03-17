87197b49b719cec629f1c52f29e1d1002ef62aa6
Anthony Capobianco <a.capobianco@stag.technofuturtic.education>
2025-03-17 15:36:17 +0100

    `operator ==` handling strings.

--- Bank/Account.cs
+++ Bank/Account.cs
@@ -8,4 +8,9 @@ public class Account(in string number) {
   
   public override string ToString() => $"Account Number: {Number}\nBalance: {_balance}:";
   public override bool Equals(object? obj) => obj is Account a && Number == a.Number;
+  public static bool operator ==(in Account self, object? o) => o switch {
+      string s => self.Number == s,
+      _ => self.Equals(o),
+  };
+  public static bool operator !=(in Account self, object? o) => !(self == o);
 }
