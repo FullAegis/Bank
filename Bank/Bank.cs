7b9918a274307ee9208cf87f19821b6873dd869a
Anthony Capobianco <a.capobianco@stag.technofuturtic.education>
2025-03-17 15:49:34 +0100

    added indexer, Add method to add a CurrentAccount

--- Bank/Bank.cs
+++ Bank/Bank.cs
@@ -1,8 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Bank;
 public class Bank(in string name) {
   public string Name { get; init; } = name;
+  private HashSet<CurrentAccount> _currentAccounts = new();
   
+  public CurrentAccount this[string number] {
+    get => _currentAccounts.First(a => a == number);
+  }
+  
+  public void Add(in CurrentAccount account) {
+    _currentAccounts.Add(account);
+  }
 }
 
