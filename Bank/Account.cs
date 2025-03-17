73a51f91b8c4c97d043aad1a08759f33a1624df0
Anthony Capobianco <a.capobianco@stag.technofuturtic.education>
2025-03-17 15:27:18 +0100

    FIX: `private` is not accessible from derived class. Changed to `protected`. Added lock for future use. Added ToString() override.

--- Bank/Account.cs
+++ Bank/Account.cs
@@ -2,10 +2,8 @@ namespace Bank;
 
 public class Account(in string number) {
   public string Number { get; init; } = number;
-  private decimal _balance = 0.0m;
-  
-  public override string ToString()
-    => $"Account Number: {Number}\nBalance: {_balance}:";
+  protected decimal _balance = 0.0m;
+  protected Lock _lock = new();
   
   public static bool Equals(in Account account1, in Account account2) {
     
