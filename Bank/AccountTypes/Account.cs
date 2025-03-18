db2ca00e681422ec252ca5adf18db08f37cdf018
Anthony Capobianco <a.capobianco@stag.technofuturtic.education>
2025-03-18 01:12:24 +0100

    CHG: Owner is now `Users.Person` (was `Users.User` which is abstract and doesn't really make sense as is)

--- Bank/AccountTypes/Account.cs
+++ Bank/AccountTypes/Account.cs
@@ -4,9 +4,9 @@ namespace Bank.AccountTypes;
 public abstract class Account {
   public string Number { get; protected init; }
   public decimal Balance { get; protected set; }
-  public User Owner { get; protected set; }
+  public Person Owner { get; protected set; }
   
-  protected Account(in string number, decimal balance, User owner) {
+  protected Account(in string number, decimal balance, Person owner) {
     Number = number;
     Balance = balance;
     Owner = owner;
