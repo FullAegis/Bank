992d6d396a3d83c3b8c4a744f99ca9c181efc55e
Anthony Capobianco <a.capobianco@stag.technofuturtic.education>
2025-03-18 01:10:09 +0100

    FIX: ctors ADD: Method deposit

--- Bank/AccountTypes/CurrentAccount.cs
+++ Bank/AccountTypes/CurrentAccount.cs
@@ -1,10 +1,35 @@
 using System;
+using System.Threading.Tasks;
 using Bank.Users;
 
 namespace Bank.AccountTypes;
 public class CurrentAccount : Account {
-  public CurrentAccount(in string number) : base(number, 0m, Person.None) {}
-  
+  public CurrentAccount(in string number, Person owner, in decimal balance = 0m)
+    : base(number, balance, owner)
+  {}
+  public CurrentAccount(in string number, in decimal balance = 0m)
+    :this(number, Person.None, balance)
+  {}
+
+  private readonly object _lock = new();
+  public override decimal Deposit(in decimal amount) {
+    // I can't have a `const long sum = decimal.ToOACurrency(amount)`. Local function it is.
+    // ToOACurrency(x) -> (long)(x * 10_000)
+    static long read(in decimal x) => decimal.ToOACurrency(x);
+    static decimal write(long x) => decimal.FromOACurrency(x);
+    var sum = read(amount);
+    if (sum < 0)
+      throw new ArgumentOutOfRangeException("amount", "Cannot deposit negative amount.");
+      
+    long newBalance;
+    lock (_lock) {
+      newBalance = read(Balance);  // Read
+      newBalance += sum;           // Modify
+      Balance = write(newBalance); // Write
+    }
+    return newBalance;
+  }
+
   public override string ToString() => $"""
     Current Account Number: {Number}
     Account Holder: {Owner.ToString()}
