65d164326ff847f2a84df0af63b1d099289556c3
Anthony Capobianco <a.capobianco@stag.technofuturtic.education>
2025-03-20 12:03:08 +0100

    MOD: Simplified Person.None ADD: Ctor taking Date as string.

--- Bank/Users/Person.cs
+++ Bank/Users/Person.cs
@@ -5,14 +5,14 @@ namespace Bank.Users;
 using System; // DateTime
 
 public sealed class Person(string firstname, string lastname, DateTime birthday) : User {
+  public static readonly Person None = new("NOT A PERSON", "N/A", DateTime.UnixEpoch);
+  
   public string FirstName { get; set; } = firstname;
   public string LastName { get; set; } = lastname;
   public DateTime Birthday { get; init; } = birthday;
 
-  private Person() : this("NOT A PERSON", "N/A", DateTime.UnixEpoch) {
-  }
-  public static readonly Person None = new();
-
-  public override string ToString()
-    => $"{LastName.ToUpper()} {FirstName} ({Birthday.Date:s})"; 
+  public Person(string firstname, string lastname, string birthday)
+    : this(firstname, lastname, DateTime.Parse(birthday, Currency.Locale))
+  {}
+  public override string ToString() => $"{LastName.ToUpper()} {FirstName} ({Birthday.Date:s})"; 
 }
