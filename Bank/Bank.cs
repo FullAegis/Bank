6ecb5a39cb6141b191a3b9fc3ddbecbdffc896bc
Anthony Capobianco <a.capobianco@stag.technofuturtic.education>
2025-03-21 11:51:27 +0100

    MOD: Made Name required

--- Bank/Bank.cs
+++ Bank/Bank.cs
@@ -5,7 +5,7 @@ using Bank.AccountTypes;
 
 namespace Bank;
 public class Bank(in string name) {
-  public string Name { get; init; } = name;
+  public required string Name { get; init; } = name;
   private readonly HashSet<CurrentAccount> _accounts = new();
   
   public CurrentAccount this[string number] => _accounts.FirstOrDefault(a => a.Equals(number));
