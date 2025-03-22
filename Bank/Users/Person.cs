416894285f0199cce1bc88e57968eb02eea18d63
Anthony Capobianco <liq4tdot@duck.com>
2025-03-22 12:21:57 +0100

    MOD: Code cleanup

--- Bank/Users/Person.cs
+++ Bank/Users/Person.cs
@@ -3,6 +3,10 @@ using Bank.AccountTypes;
 
 namespace Bank.Users;
 public sealed class Person(string firstname, string lastname, DateTime birthday) : User {
+  public string FirstName { get; set; } = firstname;
+  public string LastName { get; set; } = lastname;
+  public DateTime Birthday { get; init; } = birthday;
+
   public static readonly Person None = new("NOT A PERSON", "N/A", DateTime.UnixEpoch);
   
   public string FirstName { get; set; } = firstname;
